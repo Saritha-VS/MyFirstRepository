65a83fdb3e5257b5736bc347e272f142cb4ed4f3
Saritha-VS <saritha_vs@yahoo.com>
2015-04-11 15:17:57 -0700

    Revert "PythonTest2"
    This reverts commit 3e707641d6f87f6e7484d2756a97a747c81b0e66.

--- PythonBasicTest2.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get("https://facebook.com")
-browser.find_element_by_id("email").send_keys("username")
-browser.find_element_by_id("pass").send_keys("password")
-browser.find_element_by_id("loginbutton").click()
-browser.implicitly_wait(10)
